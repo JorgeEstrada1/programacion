cdeafb468a40fb421c5833763453afc7a9e14c4b
JorgeEstrada <geometrykoko@gmail.com>
2020-09-29 17:17:21 -0400

    Ejercicio 5

---
+++ Variables/Ejercicio 5.py
@@ -0,0 +1,7 @@
+horas_trabajadas = int(input("Ingrese cantidad horas trabajadas: "))
+pago_por_hora =float(input("Ingrese pago por hora :  "))
+monto_a_pagar = horas_trabajadas * pago_por_hora
+mensaje = "Monto total a pagar: " + str(monto_a_pagar)
+print(mensaje)
+
+
