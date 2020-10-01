560845ab0cd9c5e5c0eb71767b1e3f1dacddbcc0
JorgeEstrada <geometrykoko@gmail.com>
2020-10-01 16:30:58 -0400

    ejercicio 3

---
+++ Variables 2/Ejercicio 3.py
@@ -0,0 +1,5 @@
+radio_circulo = float(input("indique el valor del radio: "))
+area = radio_circulo * radio_circulo * 3.1416
+perimetro = 2 * 3.1416 * radio_circulo
+mensaje = f"el circulo que tiene por radio {radio_circulo}el valor de su area del circulo es {area} y su perimetro es {perimetro}"
+print(mensaje)
