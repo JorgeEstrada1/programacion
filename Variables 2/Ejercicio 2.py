82adeef24bfd32e29b06331bec0294225698c31e
JorgeEstrada <geometrykoko@gmail.com>
2020-10-01 16:22:35 -0400

    ejercicio 2

---
+++ Variables 2/Ejercicio 2.py
@@ -0,0 +1,5 @@
+valor_dividendo = int(input("indique el numero para dividir: "))
+valor_divisor =  int(input("Dividido por: "))
+resto = valor_dividendo % valor_divisor
+mensaje = f"el resto de la division de {valor_dividendo} entre {valor_divisor} es ¨{resto}"
+print(mensaje)
