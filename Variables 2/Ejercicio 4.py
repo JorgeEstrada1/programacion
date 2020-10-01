2d8022e61984c55db9e79f38de8ca1d9d96ca6bb
JorgeEstrada <geometrykoko@gmail.com>
2020-10-01 17:09:51 -0400

    ejercicio 4

---
+++ Variables 2/Ejercicio 4.py
@@ -0,0 +1,13 @@
+num_1 = int(input("ingrese primer numero: "))
+num_2 = int(input("ingrese segundo numero: "))
+son_iguales = num_1 == num_2
+print(f"Los numeros son iguales: {son_iguales}")
+
+son_distintos = num_1 != num_2
+print(f"Los numeros son distintos: {son_distintos}")
+
+es_mayor = num_1 > num_2
+print(f"El primer numero es mayor: {es_mayor}")
+
+es_menor_o_igual = num_1 <= num_2
+print(f"El primer numero es menor o igual: {es_menor_o_igual} ")
