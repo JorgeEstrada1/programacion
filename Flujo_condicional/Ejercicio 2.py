e68dd5af3cab65ab69037d4e20ce95364489d9b1
JorgeEstrada <geometrykoko@gmail.com>
2020-10-02 16:21:18 -0400

    Ejercicio 2

---
+++ Flujo_condicional/Ejercicio 2.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Indique primer numero: "))
+num_2 = int(input("Indique el segundo numero: "))
+if num_1>num_2:
+    print(f"El numero mayor es {num1}")
+else:
+    print(f"El numero mayor es {num_2}")
