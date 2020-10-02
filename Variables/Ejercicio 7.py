f69772f1c09133324a14466fa6be3812f654a939
JorgeEstrada <geometrykoko@gmail.com>
2020-10-02 09:23:29 -0400

    Ejercicio 7

---
+++ Variables/Ejercicio 7.py
@@ -0,0 +1,6 @@
+num = int(input("ingrese un numero: "))
+print (f"su numero entero es: {num}")
+resultado = (num + 1) * num
+resultado = resultado / 2
+mensaje = f"Tu resultado es: {resultado}"
+print(mensaje)
