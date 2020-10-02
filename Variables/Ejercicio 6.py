96a9e65b4835996efb7d9ccf1935947bba72457e
JorgeEstrada <geometrykoko@gmail.com>
2020-10-02 09:14:10 -0400

    Ejercicio 6

---
+++ Variables/Ejercicio 6.py
@@ -0,0 +1,5 @@
+valor_peso = int(input("Indica cuanto tu peso: "))
+valor_estatura = float(input("Indica cuanto mides: "))
+Masa_corporal = valor_peso * valor_estatura
+mensaje = f"El indice de tu mas corporal es de {Masa_corporal}"
+print(mensaje)
