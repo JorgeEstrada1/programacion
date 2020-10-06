91d0fcece1228c9853bc7453764b671ee1ca19a4
JorgeEstrada <geometrykoko@gmail.com>
2020-10-06 14:21:41 -0400

    Ejercicio 5

---
+++ Flujo_condicional/Ejercicio 5.py
@@ -0,0 +1,11 @@
+numero = int(input("Que numero: "))
+if numero == 0:
+    print("Su numero es cero")
+else:
+    if numero > 0:
+        print("Su numero es positivo")
+    else:
+        print("Su numero es negativo")
+
+
+
