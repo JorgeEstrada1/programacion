e29e9689987b678163eecde6e8445046079d19e2
JorgeEstrada <geometrykoko@gmail.com>
2020-10-09 17:12:15 -0400

    Ejercicio 8

---
+++ Practico_1/Ejercicio 8.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("Ingresa un numero: "))
+num3= 0
+for i in range (num1+1,num2):
+    i = sum(range(num1+1,num2))
+    num3 = i
+print(f"la suma es {i}")
