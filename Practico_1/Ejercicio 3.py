2f29f45cdc5d247ee6ff0739f13b0f208756bcd1
JorgeEstrada <geometrykoko@gmail.com>
2020-10-13 11:29:01 -0400

    Ejercicio 3

--- Practico_1/Ejercicio 3.py
+++ Practico_1/Ejercicio 3.py
@@ -1,8 +1,6 @@
 dividendo = int(input("Escriba el dividendo: "))
 divisor = int(input("Escriba el divisor: "))
 if dividendo % divisor == 0:
-    print(f"La division es exacta. cociente: {dividendo//divisor} resto: {dividendo%divisor}")
+    print(f"La division es exacta. Cociente: {dividendo//divisor} Resto: {dividendo%divisor}")
 else:
-    print(f"La division no es exacta. Cociente: {dividendo//divisor} resto: {dividendo%divisor}")
-
-
+    print(f"La division no es exacta. Cociente: {dividendo//divisor} Resto: {dividendo%divisor}")
