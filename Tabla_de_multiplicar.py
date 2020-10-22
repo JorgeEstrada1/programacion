64dd6a65b6d45df97346fbb9fad0dc95d41f4434
JorgeEstrada <geometrykoko@gmail.com>
2020-10-22 16:20:59 -0400

    Tabla de multiplicar

---
+++ Tabla_de_multiplicar.py
@@ -0,0 +1,18 @@
+#tablas de multiplicar
+def tablaM(num):
+    print("Tabla de multiplicar del", num)
+    for i in range (1,11):
+        print(num, " x ", i , " = ", i * num)
+
+
+num=tablaM(1)
+num=tablaM(2)
+num=tablaM(3)
+num=tablaM(4)
+num=tablaM(5)
+num=tablaM(6)
+num=tablaM(7)
+num=tablaM(8)
+num=tablaM(9)
+num=tablaM(10)
+
