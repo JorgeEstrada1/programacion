f2ce7e19c96fb9e7ca7d27dd473c22740c75ac87
JorgeEstrada <geometrykoko@gmail.com>
2020-10-27 16:28:57 -0400

    Ejercicio 4

--- Practico/Ejercicio 4.py
+++ Practico/Ejercicio 4.py
@@ -1,5 +1,5 @@
 def word(x):
-    if x== "a" or x== "e" or x== "i" or x== "o" or x== "u":
+    if x== "a" or x== "e" or x== "i" or x== "o" or x== "u" or x== "A" or x== "E" or x== "I" or x== "O" or x== "U":
         print("True")
     else:
         print("False")
