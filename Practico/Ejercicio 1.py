da818b89da2f798ac9ff28fd2828010a62129900
JorgeEstrada <geometrykoko@gmail.com>
2020-10-27 13:01:01 -0400

    ejercicio 1

---
+++ Practico/Ejercicio 1.py
@@ -0,0 +1,6 @@
+def nummax(num1,num2):
+    if num1 > num2:
+        print("EL numero", num1, "es mayor que el numero", num2 )
+    else:
+        print("EL numero", num2, "es mayor que el numero", num1 )
+nummax(int(input("ingrese un numero: ")),int(input("ingrese un numero: ")))
