4c07f18336e8d9686d3e4856f87ea6e698029685
JorgeEstrada <geometrykoko@gmail.com>
2020-11-27 16:49:45 -0400

    Ejercicio 3

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -2,14 +2,15 @@ import random
 numero=[]
 array=[]
 tamaño=int(input("ingrese el tamaño de su array: "))
+digito=input("ingrese el ultimo digito para verificar: ")
 for i in range(0,tamaño):
     num= random.randint(0,300)
     array.append(num)
     num=str(num)
     for i in range(0,tamaño):
         ultimo_digito= num[len(num)-1]
-    if ultimo_digito=="5":
-        numeros_correcto.append(num)
+    if ultimo_digito==digito:
+        numero.append(num)
 
 print(numero)
 print(array)
